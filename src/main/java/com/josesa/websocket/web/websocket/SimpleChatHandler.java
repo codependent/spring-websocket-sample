5f8bd84f76ed4bea6ae55a698b4e3be70f69b9f9
codependent <joseantonio.inigo@gmail.com>
2014-04-09 12:53:17 +0200

    update1

--- src/main/java/com/josesa/websocket/web/websocket/SimpleChatHandler.java
+++ src/main/java/com/josesa/websocket/web/websocket/SimpleChatHandler.java
@@ -6,11 +6,15 @@ import java.util.Map.Entry;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class SimpleChatHandler extends TextWebSocketHandler{
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
 	
 	@Resource(name="simpleChatSessionMap")
 	private Map<String, WebSocketSession> simpleChatSessionMap;
@@ -18,18 +22,22 @@ public class SimpleChatHandler extends TextWebSocketHandler{
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		
+		logger.info("received message {}",message.getPayload());
+		
 		if(!simpleChatSessionMap.containsKey(session.getId())){
+			logger.info("adding new session {}",session.getId());
 			simpleChatSessionMap.put(session.getId(), session);
 		}
 		String userName = (String)session.getAttributes().get("userName");
-		System.out.println("chat recibido "+message.getPayload());
 		TextMessage tm = new TextMessage((userName + ": "+message.getPayload()));
 
 		for (Iterator<Entry<String, WebSocketSession>> iterator = simpleChatSessionMap.entrySet().iterator(); iterator.hasNext();) {
 			WebSocketSession s = iterator.next().getValue();
 			if(s.isOpen()){
+				logger.info("sending message to WebSocketSession {}",s.getId());
 				s.sendMessage(tm);
 			}else{
+				logger.info("removing closed session {}",s.getId());
 				iterator.remove();
 			}
 		}
